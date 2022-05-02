af3fecb30af66f0c2815d07eccb7e36221d70793
AaronTheNerd <bargeaaron@gmail.com>
2022-05-02 07:44:01 -0400

    Simplified main method

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,16 @@
 
 int main(int argc, char **argv) {
     atn::Wally target;
-    std::map<char, atn::Color> bin_key {
+    std::map<char, atn::Color> key {
         {'0', atn::ANY},
-        {'1', atn::ANY}
-    };
-    std::map<char, atn::Color> b4_key {
-        {'0', atn::ANY},
-        {'1', atn::ANY}
+        {'1', atn::ANY},
+        {'2', atn::ANY},
+        {'3', atn::ANY}
     };
     std::cout << target.wally << std::endl;
-    for (size_t width = target.width(); width < 1000; ++width) {
-        atn::SearchResult search_result = atn::search_pi(target, width, "data/pi_bin_1b.txt", bin_key);
-        if (search_result.result != 0) {
+    for (size_t width = target.width(); width < 100; ++width) {
+        atn::SearchResult search_result = atn::search_pi(target, width, "data/pi_bin_1b.txt", key);
+        if (search_result.result) {
             std::cout << "Test succeeded!" << std::endl;
             std::cout << "  Width: " << width << std::endl;
             std::cout << "  Position: " << search_result.pos << std::endl;
